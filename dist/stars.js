c0201cd39f56b805b0584b609f8a2986daf50885
Carolina Vargas <72519137+carolinavargasl@users.noreply.github.com>
2024-01-04 14:42:12 -0500

    w

---
+++ dist/stars.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Cargar la animación
+    var animacion = bodymovin.loadAnimation({
+        container: document.getElementById('animacion'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'RS/build/js/home/stars.json' // Ruta al archivo JSON
+    });
+});
+
+
+//# sourceMappingURL=stars.js.map
